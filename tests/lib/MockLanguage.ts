c7fe7809e5f837bf2749869e03b292d2663e7a8b
Antonio Román <kyradiscord@gmail.com>
2020-08-19 15:30:33 +0200

    fix: use name instead of key for MINMAX

--- tests/lib/MockLanguage.ts
+++ tests/lib/MockLanguage.ts
@@ -16,23 +16,23 @@ export class MockLanguage extends Language {
 				`[SETTING_GATEWAY_DUPLICATE_VALUE]: ${entry.path} ${value}`,
 			SETTING_GATEWAY_INVALID_FILTERED_VALUE: ({ entry, value }: { entry: SchemaEntry; value: string }): string =>
 				`[SETTING_GATEWAY_INVALID_FILTERED_VALUE]: ${entry.path} ${value}`,
-			RESOLVER_MINMAX_EXACTLY: ({ key, value, inclusive }: { key: string; value: number; inclusive: boolean }): string =>
-				`[RESOLVER_MINMAX_EXACTLY]: ${key} ${value} ${inclusive}`,
+			RESOLVER_MINMAX_EXACTLY: ({ name, value, inclusive }: { name: string; value: number; inclusive: boolean }): string =>
+				`[RESOLVER_MINMAX_EXACTLY]: ${name} ${value} ${inclusive}`,
 			RESOLVER_MINMAX_BOTH: ({
-				key,
+				name,
 				minimum,
 				maximum,
 				inclusive
 			}: {
-				key: string;
+				name: string;
 				minimum: number;
 				maximum: number;
 				inclusive: boolean;
-			}): string => `[RESOLVER_MINMAX_BOTH]: ${key} ${minimum} ${maximum} ${inclusive}`,
-			RESOLVER_MINMAX_MIN: ({ key, minimum, inclusive }: { key: string; minimum: number; inclusive: number }): string =>
-				`[RESOLVER_MINMAX_MIN]: ${key} ${minimum} ${inclusive}`,
-			RESOLVER_MINMAX_MAX: ({ key, maximum, inclusive }: { key: string; maximum: number; inclusive: number }): string =>
-				`[RESOLVER_MINMAX_MAX]: ${key} ${maximum} ${inclusive}`
+			}): string => `[RESOLVER_MINMAX_BOTH]: ${name} ${minimum} ${maximum} ${inclusive}`,
+			RESOLVER_MINMAX_MIN: ({ name, minimum, inclusive }: { name: string; minimum: number; inclusive: number }): string =>
+				`[RESOLVER_MINMAX_MIN]: ${name} ${minimum} ${inclusive}`,
+			RESOLVER_MINMAX_MAX: ({ name, maximum, inclusive }: { name: string; maximum: number; inclusive: number }): string =>
+				`[RESOLVER_MINMAX_MAX]: ${name} ${maximum} ${inclusive}`
 		};
 	}
 }
