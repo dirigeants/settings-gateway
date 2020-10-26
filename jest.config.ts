d8679a863a3192ebe993bea6f2b1bfab9e882ddb
Jeroen Claassens <support@favware.tech>
2020-10-26 17:34:36 +0100

    test: ts-ify jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,7 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async (): Promise<Config.InitialOptions> => ({
 	displayName: 'unit test',
 	preset: 'ts-jest',
 	testEnvironment: 'node',
@@ -6,7 +9,7 @@ module.exports = {
 	testMatch: ['<rootDir>/tests/**/*.test.ts'],
 	globals: {
 		'ts-jest': {
-			tsConfig: '<rootDir>/tests/tsconfig.json'
+			tsconfig: '<rootDir>/tests/tsconfig.json'
 		}
 	},
 	coveragePathIgnorePatterns: [
@@ -18,4 +21,4 @@ module.exports = {
 		'<rootDir>/tests/lib/MockStringSerializer.ts',
 		'<rootDir>/tests/lib/Util.ts'
 	]
-};
+});
