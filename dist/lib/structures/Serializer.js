5d22efe1ec6a292d412f38827b6ccda5218a683f
Favna <contact@skyra.pw>
2020-10-30 20:56:42 +0000

    build: tsc build for 46165f8dd8cf7f82d6fc1db77a40a73922bffe24

--- dist/lib/structures/Serializer.js
+++ dist/lib/structures/Serializer.js
@@ -57,18 +57,18 @@ class Serializer extends klasa_1.AliasPiece {
             if ((value >= minimum && value <= maximum && inclusive) || (value > minimum && value < maximum && !inclusive))
                 return true;
             if (minimum === maximum)
-                throw new RangeError(language.get('resolverMinmaxExactly', { key, min: minimum, inclusive }));
-            throw new RangeError(language.get('resolverMinmaxBoth', { key, min: minimum, max: maximum, inclusive }));
+                throw new RangeError(language.get(inclusive ? 'resolverMinmaxExactlyInclusive' : 'resolverMinmaxExactlyExclusive', { name: key, min: minimum }));
+            throw new RangeError(language.get(inclusive ? 'resolverMinmaxBothInclusive' : 'resolverMinmaxBothExclusive', { name: key, min: minimum, max: maximum }));
         }
         else if (minimum) {
             if ((value >= minimum && inclusive) || (value > minimum && !inclusive))
                 return true;
-            throw new RangeError(language.get('resolverMinmaxMin', { key, min: minimum, inclusive }));
+            throw new RangeError(language.get(inclusive ? 'resolverMinmaxMinInclusive' : 'resolverMinmaxMinExclusive', { name: key, min: minimum }));
         }
         else if (maximum) {
             if ((value <= maximum && inclusive) || (value < maximum && !inclusive))
                 return true;
-            throw new RangeError(language.get('resolverMinmaxMax', { key, max: maximum, inclusive }));
+            throw new RangeError(language.get(inclusive ? 'resolverMinmaxMaxInclusive' : 'resolverMinmaxMaxExclusive', { name: key, max: maximum }));
         }
         return true;
     }
